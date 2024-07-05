88deafb6a80653f7bc2e5b12af020deff3fe1a6e
KhajiraCraft <khajirachris@gmail.com>
2024-07-05 08:03:43 -0400

    Wrapped the Quiz component in the provider componenet

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Quiz from './Components/Quiz';
-
+import { QuizProvider } from './contexts/quiz';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <QuizProvider>
     <Quiz />
+    </QuizProvider>
+    
   </React.StrictMode>
 );
 
